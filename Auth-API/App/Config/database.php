26c1bbb509bb9f3672481455f6eb88d965231c52
Hendrew Gustavo <113458822+Hendr3w@users.noreply.github.com>
2025-06-03 20:52:00 -0300

    Troca de senha em database.php

--- Auth-API/App/Config/database.php
+++ Auth-API/App/Config/database.php
@@ -8,13 +8,12 @@ class Database {
     public function connect(): PDO {
         return new PDO(
             'mysql:host=mysql;port=3306;dbname=auth_api;charset=utf8mb4',
-            'auth_user',
-            'auth_pass',
+            'root',
+            'senha', 
             [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             ]
         );
-        
     }
 }
